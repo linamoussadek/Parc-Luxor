b76fc44da5a3582f4ad8bfd3c890813d14034efb
linamoussadek <lmous090@uottawa.ca>
2024-08-08 12:49:46 -0400

    updated logos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Component } from './components/Component';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Component />} />
-          {/* Define other routes as needed */}
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Component />} />
+                {/* Define other routes as needed */}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
